204ad023c77e0bd25726f85aaad55fdb3775dbd0
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-01-22 17:56:00 +0100

    Add some helper physics functions

--- src/physics.rs
+++ src/physics.rs
@@ -1,4 +1,5 @@
 pub struct PhysObject{
+    body_name:String,
     pos: [f64;2],
     vel: [f64;2],
     mass: f64,
@@ -15,6 +16,60 @@ pub fn distance(a : &PhysObject, b : &PhysObject) -> f64 {
     ((a.pos[0]-b.pos[0]).powi(2) + (a.pos[1]-b.pos[1]).powi(2)).sqrt()
 }
 
+fn vector_length(vector : [f64;2])->f64{
+    (vector[0]*vector[0] + vector[1]*vector[1]).sqrt()
+}
+
+fn normalize_vector(vector : [f64;2])-> [f64;2]{
+    let vector_length = vector_length(vector);
+    [vector[0]/vector_length, vector[1]/vector_length]
+}
+
+impl PhysObject{
+    fn apply_force(&mut self, force : [f64;2]){
+        self.vel[0] += force[0]/self.mass;
+        self.vel[1] += force[1]/self.mass;
+    }
+
+    fn update(&mut self, dtime : f64){
+        self.pos[0] += self.vel[0]*dtime;
+        self.pos[1] += self.vel[1]*dtime;
+    }
+}
+
+pub fn update_bodies(bodies: &mut [PhysObject], dt: f64){
+
+    for i in 0..bodies.len(){
+        for j in 0..bodies.len(){
+            if i==j{
+                continue;
+            }
+
+            let (body1, body2) = {
+                let (left, right) = if i < j {
+                    bodies.split_at_mut(j)
+                } else {
+                    bodies.split_at_mut(i)
+                };
+
+                if i < j {
+                    (&left[i], &mut right[0])
+                } else {
+                    (&right[0], &mut left[j])
+                }
+            };
+
+            let force = get_gravitational_force(body1, body2) * dt;
+            let mut direction = [
+                body2.pos[0]-body1.pos[0],
+                body2.pos[1]-body1.pos[1]
+            ];
+            direction = normalize_vector(direction);
+            body2.apply_force([direction[0]*force,direction[1]*force]);
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -23,12 +78,14 @@ mod tests {
     #[test]
     fn test_distance() {
         let obj1 = PhysObject{
+            body_name: String::from("Body1"),
             pos: [0.0,0.0],
             vel: [0.0,0.0],
             mass: 5.9722e24,
         };
 
         let obj2 = PhysObject{
+            body_name: String::from("Body2"),
             pos: [2.5,2.5],
             vel: [0.0,0.0],
             mass: 1.988416e30,
@@ -41,12 +98,14 @@ mod tests {
     #[test]
     fn test_gravitational_force() {
         let obj1 = PhysObject{
+            body_name: String::from("Body1"),
             pos: [0.0, 1.495978707e11],
             vel: [0.0,0.0],
             mass: 5.9722e24,
         };
 
         let obj2 = PhysObject{
+            body_name: String::from("Body2"),
             pos: [0.0,0.0],
             vel: [0.0,0.0],
             mass: 1.988416e30,
@@ -55,6 +114,24 @@ mod tests {
         let force = get_gravitational_force(&obj1, &obj2);
         let expected_force = 3.54e22;
         assert!((force - expected_force).abs()/expected_force < 0.001);
+    }
+
+    #[test]
+    fn test_vector_length() {
+        let vector1 = [0.0,0.0];
+        let vector2 = [0.0,1.0];
+        let vector3 = [3.0,3.0];
+        let vector_length1 = vector_length(vector1);
+        let vector_length2 = vector_length(vector2);
+        let vector_length3 = vector_length(vector3);
+
+        let expected_length1 = 0.0;
+        let expected_length2 = 1.0;
+        let expected_length3 = 4.24264;
+
+        assert!((vector_length1-expected_length1).abs() < 0.01);
+        assert!((vector_length2-expected_length2).abs() < 0.01);
+        assert!((vector_length3-expected_length3).abs() < 0.01);
     }
 
 }
